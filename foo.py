2d52186f18578729e3e68775beef5fc26c20beee
Varun Munjeti <vrunjeti@gmail.com>
2015-11-21 19:42:45 -0600

    meep

--- foo.py
+++ foo.py
@@ -59,3 +59,54 @@ def generate_scale(name, octave, major=True):
     scale = major_scale_progression if major else minor_scale_progression
     base_note = note_value(name+str(octave))
     return [ base_note + x for x in scale ]
+
+transitions_major = {
+  'I': ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'vii0'],
+  'ii': ['V', 'vii0'],
+  'iii': ['IV', 'vi'],
+  'IV': ['I', 'ii', 'V', 'vii0'],
+  'V': ['I', 'vi'],
+  'vi': ['ii', 'IV', 'V'],
+  'vii0': ['I']
+}
+
+transitions_minor = {
+  'i': ['i', 'ii0', 'III', 'iv', 'V', 'VI', 'VII', 'vii0'],
+  'ii0': ['V', 'vii0'],
+  'III': ['iv', 'VI'],
+  'iv': ['i', 'ii0', 'V', 'vii0'],
+  'V': ['i', 'VI'],
+  'VI': ['ii0', 'iv', 'V'],
+  'VII': ['III'],
+  'vii0': ['i', 'V']
+}
+
+notes = {
+  'I': [0, 4, 7],
+  'II': [2, 6, 9],
+  'III': [4, 8, 11],
+  'IV': [5, 9, 12],
+  'V': [7, 11, 14],
+  'VI': [0, 9, 13],
+  # 'VI': [9, 13, 16],
+  'VII': [2, 11, 15],
+  # 'VII': [11, 15, 18],
+  'i': major_chord_to_minor('I'),
+  'ii': major_chord_to_minor('II'),
+  'iii': major_chord_to_minor('III'),
+  'iv': major_chord_to_minor('IV'),
+  'v': major_chord_to_minor('V'),
+  'vi': major_chord_to_minor('VI'),
+  'vii': major_chord_to_minor('VII'),
+  'ii0': [1, 4, 7, 9],
+  'vii0': [0, 4, 7, 10]
+}
+
+def major_chord_to_minor(symbol):
+  minor = notes[symbol]
+  minor[1] -= 1
+  return minor
+
+def get_chord(symbol, base_note):
+  return [notes[symbol] + base_note for i in range(len(notes[symbol]))]
+
